d0386e12e3a0d239197e59ae20f5979ef2bc9e4d
Bruno Beildeck <bbeildec.k@gmail.com>
2015-07-07 01:18:58 -0300

    Add expense create endpoint

--- Outlay/Modules/ExpenseModule.cs
+++ Outlay/Modules/ExpenseModule.cs
@@ -4,46 +4,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Nancy.ModelBinding;
 
 namespace Outlay.Modules
 {
     public class ExpenseModule : NancyModule
     {
+        public List<Expense> expenses = new List<Expense>
+                {
+                    new Expense { Id = 1, Value = 5000, Category = ExpenseCategory.Artist, Description = "Tiesto"},
+                    new Expense { Id = 2, Value = 500, Category = ExpenseCategory.Electricity},
+                    new Expense { Id = 3, Value = 3000, Category = ExpenseCategory.Liquor, Description = "Black label"},
+                    new Expense { Id = 4, Value = 1000, Category = ExpenseCategory.Liquor, Description = "Grey Goose"}
+                };
+
         public ExpenseModule()
         {
-            Get["/status"] = _ => "Hello..";
+            Get["/"] = _ => "Oh, hai.";
 
-            Get["/expense/get"] = p =>
+            Get["/status"] = _ => HttpStatusCode.OK;
+
+            Get["/expense"] = p =>
             {
-                var expenses = new Expense[]
-                {
-                    new Expense { Id = 1, Value = 5000, Category = ExpenseCategory.Artist, Description = "Tiesto"},
-                    new Expense { Id = 2, Value = 500, Category = ExpenseCategory.Electricity},
-                    new Expense { Id = 3, Value = 3000, Category = ExpenseCategory.Liquor, Description = "Black label"},
-                    new Expense { Id = 4, Value = 1000, Category = ExpenseCategory.Liquor, Description = "Grey Goose"}
-                };
-
-                return Response.AsJson<IEnumerable<Expense>>(expenses);
+                return Response.AsJson(expenses);
             };
 
-            Get["/expense/get/{id}"] = p =>
+            Get["/expense/{id}"] = p =>
             {
-                var expenses = new Expense[]
-                {
-                    new Expense { Id = 1, Value = 5000, Category = ExpenseCategory.Artist, Description = "Tiesto"},
-                    new Expense { Id = 2, Value = 500, Category = ExpenseCategory.Electricity},
-                    new Expense { Id = 3, Value = 3000, Category = ExpenseCategory.Liquor, Description = "Black label"},
-                    new Expense { Id = 4, Value = 1000, Category = ExpenseCategory.Liquor, Description = "Grey Goose"}
-                };
-
                 var exp = expenses.SingleOrDefault(x => x.Id == p.id);
                 if (exp == null)
                 {
-                    return Negotiate
-                        .WithStatusCode(HttpStatusCode.NotFound);
+                    return HttpStatusCode.BadRequest;
                 }
 
                 return Response.AsJson(exp);
+            };
+
+            Post["/expense"] = p =>
+            {
+                Expense exp = this.Bind();
+                expenses.Add(exp);
+
+                return Response.AsJson(expenses);
             };
         }
     }
