018cdd81bbb735efbe915509eda2824cc5730a72
Bruno Beildeck <bbeildec.k@gmail.com>
2015-07-07 01:06:52 -0300

    Create expense module with HelloWorld

---
+++ Outlay/Modules/ExpenseModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Outlay.Modules
+{
+    public class ExpenseModule : NancyModule
+    {
+        public ExpenseModule()
+        {
+            Get["/"] = parameters => "Hello..";
+        }
+    }
+}
