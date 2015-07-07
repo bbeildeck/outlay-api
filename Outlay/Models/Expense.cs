c07a7d5596e5034841dd6fe8e69a4d2995776ed9
Bruno Beildeck <bbeildec.k@gmail.com>
2015-07-07 00:54:47 -0300

    Create basic expense data model

---
+++ Outlay/Models/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Outlay.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public double Value { get; set; }
+        public string Description { get; set; }
+        public ExpenseCategory Category { get; set; }
+    }
+}
